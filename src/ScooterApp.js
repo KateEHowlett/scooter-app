0ef50f7588a6af6ac3b5b743bae42fbdfb30d1c2
Kate Howlett <kate.howlett@norfolk.gov.uk>
2023-07-28 13:07:30 +0100

    Added register user code and test

--- src/ScooterApp.js
+++ src/ScooterApp.js
@@ -3,6 +3,24 @@ const Scooter = require('./Scooter')
 
 class ScooterApp {
   // ScooterApp code here
+  constructor(){
+    this.stations = {
+      Norwich:[],
+      Ely:[],
+      Cambridge:[]
+    };
+    this.registeredUsers = {}
+  }
+
+  registerUser(username,password,age){
+    if(age<18){
+      throw new Error("too young to register")
+    } else if (this.registeredUsers.hasOwnProperty(username)){
+      throw new Error("already registered")
+    } else {
+      this.registeredUsers[username] = password;
+    }
+  }
 }
 
 module.exports = ScooterApp
--- tests/ScooterApp.test.js
+++ tests/ScooterApp.test.js
@@ -1,10 +1,25 @@
 const Scooter = require('../src/Scooter')
 const User = require('../src/User')
 const ScooterApp = require('../src/ScooterApp')
+const app1 = new ScooterApp;
 
 // ScooterApp tests here
 
 // register user
+describe("how to register a user",()=>{
+    it("allows new users",()=>{
+        expect(()=>{
+            app1.registerUser("kilobyte","megabyte",26)}).toThrow();
+    })
+    it("doesn't allow under 18",()=>{
+        expect(()=>{
+            app1.registerUser("salixa","secret",17)}).toThrow();
+        })
+    it("doesn't register existing users",()=>{
+        expect(()=>{
+            app1.registerUser("kilobyte","supersecret",26)}).toThrow();
+    })
+})
 
 // log in
 
